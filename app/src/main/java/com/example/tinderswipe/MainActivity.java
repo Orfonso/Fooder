2978c9ee91582c86a2f7a3131087ee6d79c6d6c5
Orfonso <orsonmeyreles7@gmail.com>
2018-09-16 14:59:07 -0400

    Added a header and nav button
    Stilll bugs with all drawer buttons doing the same action of opening all activities

--- app/src/main/java/com/example/tinderswipe/MainActivity.java
+++ app/src/main/java/com/example/tinderswipe/MainActivity.java
@@ -8,7 +8,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBarDrawerToggle;
+
 
 import com.mindorks.placeholderview.SwipeDecor;
 import com.mindorks.placeholderview.SwipePlaceHolderView;
@@ -23,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     private Context mContext;
 
     private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navView;
 
 
     @Override
@@ -30,10 +34,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mDrawerLayout = findViewById(R.id.drawer_layout);
-
         //Card Swipe
-        mSwipeView = (SwipePlaceHolderView)findViewById(R.id.swipeView);
+        mSwipeView = (SwipePlaceHolderView) findViewById(R.id.swipeView);
         mContext = getApplicationContext();
 
         mSwipeView.getBuilder()
@@ -45,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
                         .setSwipeOutMsgLayoutId(R.layout.tinder_swipe_out_msg_view));
 
 
-        for(Profile profile : Utils.loadProfiles(this.getApplicationContext())){
+        for (Profile profile : Utils.loadProfiles(this.getApplicationContext())) {
             mSwipeView.addView(new TinderCard(mContext, profile, mSwipeView));
         }
 
@@ -63,52 +65,74 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        final NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(
-            new NavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(MenuItem item) {
-                    // set item as selected to persist highlight
-                    item.setChecked(true);
+        //Navigation Drawer
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.openDrawer, R.string.closeDrawer);
 
-                    // close drawer when item is tapped
-                    mDrawerLayout.closeDrawers();
-                    // Add code here to update the UI based on the item selected
-                    // For example, swap UI fragments here
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
-                    int id = item.getItemId();
-                    switch(id) {
-                        case R.id.nav_home:
-                            openActivity(HOME);
-                        case R.id.nav_settings:
-                            openActivity(SETTINGS);
-                        case R.id.nav_about:
-                            openActivity(ABOUT);
-                        default:
-                            return true;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navView = findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        // set item as selected to persist highlight
+                        //item.setChecked(true);
+
+                        // close drawer when item is tapped
+                        //mDrawerLayout.closeDrawers();
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        int id = item.getItemId();
+                        switch (id) {
+                            case R.id.nav_home:
+                                openActivity(HOME);
+                            case R.id.nav_settings:
+                                openActivity(SETTINGS);
+                            case R.id.nav_about:
+                                openActivity(ABOUT);
+                            default:
+                                return true;
+                        }
                     }
-
-
                 }
-            }
         );
 
 
     }
 
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+
+            if(toggle.onOptionsItemSelected(item)) {
+                return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
+
+
+
+
+
     public void openActivity(int i) {
         Intent intent ;
 
-        if (i == HOME) {
-            intent = new Intent(this, MainActivity.class);
+        if (i == ABOUT) {
+            intent = new Intent(this, AboutActivity.class);
             startActivity(intent);
         }
         else if (i == SETTINGS) {
             intent = new Intent(this, SettingsActivity.class);
             startActivity(intent);
         }
-        else if (i == ABOUT) {
-            intent = new Intent(this, AboutActivity.class);
+        else if (i == HOME) {
+            intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         }
     }
